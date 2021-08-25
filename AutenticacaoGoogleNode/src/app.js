10d6bebc6778fef85dd55da5867773ee2acca970
Sandy Hoffmann <manglesan98@gmail.com>
2021-08-24 23:37:41 -0300

    atenticacao funcionando basico

--- AutenticacaoGoogleNode/src/app.js
+++ AutenticacaoGoogleNode/src/app.js
@@ -1,36 +1,60 @@
 const express = require('express');
-// const {OAuth2Client} = require('google-auth-library');
+const {OAuth2Client} = require('google-auth-library');
 const app = express();
-// CLIENT_ID = '398550612339-hvh64v6qohvoog82vj59rn82c1ogrol8'
-// const client = new OAuth2Client(CLIENT_ID);
-const cors = require("cors")
-
+SECRET_CLIENT_ID = '-3uNOn1K7EC4AxAR3ROW70wA'
+CLIENT_ID = '398550612339-hvh64v6qohvoog82vj59rn82c1ogrol8.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID,SECRET_CLIENT_ID);
+const cors = require("cors");
 app.use(cors({
-    origin: "http://localhost:3000/"
+    origin: "http://localhost:5501"
 }));
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Ola aaaab");
-
-    try {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-            // Or, if multiple clients access the backend:
-            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-        });
-        console.log(ticket)
-        // const payload = await ticket.getPayload();
-        // const userid = await payload['sub'];
-        // If request specified a G Suite domain:
-        // const domain = payload['hd']
-    } catch (error) {
-        console.log(error)
+async function authMiddleware(token) {
+        try {
+            const ticket = await client.verifyIdToken({
+                idToken: token,
+                audience: CLIENT_ID
+                // Specify the CLIENT_ID of the app that accesses the backend
+                // Or, if multiple clients access the backend:
+                //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+            });
+            console.log(ticket)
+            const payload = ticket.getPayload();
+    
+            return payload
+        } catch (error) {
+            return error
+        }
+        
+        
     }
-
+app.post("/", async (req, res) => {
+    let tok = req.body.token
+    let auth = await authMiddleware(tok)
+    console.log(auth)
+    res.send("aaa")
 });
+    // try {
+    //     const ticket = client.verifyIdToken({
+    //         idToken: token,
+    //         audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    //         // Or, if multiple clients access the backend:
+    //         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    //     });
+    //     console.log(ticket)
+    //     res.send("ok")
+    //     // const payload = await ticket.getPayload();
+    //     // const userid = await payload['sub'];
+    //     // If request specified a G Suite domain:
+    //     // const domain = payload['hd']
+    // } catch (error) {
+    //     res.send("nope")
+    //     console.log(error)
+    // }
+
+
 
 // app.get("/", async (req, res) => {
 //     res.send("Ola a");
@@ -56,3 +80,5 @@ app.get("/", (req, res) => {
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando em: http://localhost:${PORT}`));
+
+// https://www.youtube.com/watch?v=-yH1ZnZBWyU
